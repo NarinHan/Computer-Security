58e6686536b046065f29c38b0b58570ba9de054d
Narin Han <lynniepro21@gmail.com>
2024-12-12 17:20:36 +0900

    hw3 source code updated

--- hw3/tls_chat_clnt.c
+++ hw3/tls_chat_clnt.c
@@ -1,20 +1,29 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <unistd.h> 
 #include <string.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <pthread.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
 	
-#define BUF_SIZE 100
+#define BUF_SIZE 1024
 #define NAME_SIZE 20
 	
+#define htonll(x)   ((((uint64_t)htonl(x)) << 32) + htonl(x >> 32))
+#define ntohll(x)   ((((uint64_t)ntohl(x)) << 32) + ntohl(x >> 32))
+
 void *send_msg(void * arg);
 void *recv_msg(void * arg);
 void error_handling(char * msg);
 	
 char name[NAME_SIZE] = "[DEFAULT]";
 char msg[BUF_SIZE];
+
+SSL_CTX *ctx;
+SSL *ssl;
 	
 int main(int argc, char *argv[])
 {
@@ -24,7 +33,7 @@ int main(int argc, char *argv[])
 	void * thread_return;
 	if (argc != 4) {
 		printf("Usage : %s <IP> <port> <name>\n", argv[0]);
-		exit(1);
+		exit(EXIT_FAILURE);
 	 }
 	
 	sprintf(name, "[%s]", argv[3]);
@@ -37,11 +46,43 @@ int main(int argc, char *argv[])
 	  
 	if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
 		error_handling("connect() error");
-	
+
+    // initialize OpenSSL
+    SSL_library_init();
+    OpenSSL_add_all_algorithms(); // load all crypto algorithms
+    SSL_load_error_strings(); // load all error messages
+    const SSL_METHOD *method = TLS_client_method();
+    ctx = SSL_CTX_new(method);
+    if (!ctx) {
+        error_handling("SLS_CTX_new() error");
+    }
+
+    // load server's (self-signed) certificate as trusted CA
+    if (!SSL_CTX_load_verify_locations(ctx, "server.crt", NULL)) {
+        fprintf(stderr, "Error loading server certificate as trusted CA.\n");
+        ERR_print_errors_fp(stderr); 
+        exit(EXIT_FAILURE);
+    }
+    SSL_CTX_set_verify(ctx, SSL_VERIFY_PEER, NULL);
+    
+    ssl = SSL_new(ctx);
+    SSL_set_fd(ssl, sock);
+    if (SSL_connect(ssl) <= 0) {
+        ERR_print_errors_fp(stderr);
+        error_handling("SSL_connect() error");
+    }
+    printf("TLS handshake successful!\n");
+    printf("Cipher: %s\n", SSL_get_cipher(ssl));
+    printf("TLS Version: %s\n", SSL_get_version(ssl));
+
 	pthread_create(&snd_thread, NULL, send_msg, (void*)&sock);
 	pthread_create(&rcv_thread, NULL, recv_msg, (void*)&sock);
 	pthread_join(snd_thread, &thread_return);
 	pthread_join(rcv_thread, &thread_return);
+
+    SSL_shutdown(ssl);
+    SSL_free(ssl);
+    SSL_CTX_free(ctx);
 	close(sock);  
 	return 0;
 }
@@ -50,18 +91,67 @@ void *send_msg(void * arg)   // send thread main
 {
 	int sock = *((int*)arg);
 	char name_msg[NAME_SIZE+BUF_SIZE];
+
 	while (1) 
 	{
-		fgets(msg, BUF_SIZE, stdin);
-		if (!strcmp(msg,"q\n") || !strcmp(msg,"Q\n")) 
+        printf("%s ", name);
+        fflush(stdout);
+
+		if (!fgets(msg, BUF_SIZE, stdin)) 
+            break;
+
+		if (!strcmp(msg, "q\n") || !strcmp(msg, "Q\n")) 
 		{
+            SSL_shutdown(ssl);
+            SSL_free(ssl);
+            SSL_CTX_free(ctx);
 			close(sock);
-			exit(0);
+			exit(EXIT_SUCCESS);
 		}
-		sprintf(name_msg,"%s %s", name, msg);
-		write(sock, name_msg, strlen(name_msg));
+
+        if (strncmp(msg, "file_share:", 11) == 0) {
+            // extract file name
+            char filename[BUF_SIZE];
+            if (sscanf(msg + 11, "%s", filename) != 1) {
+                printf("Usage: file_share: <filename>\n");
+                continue;
+            }
+
+            // send the command line first to inform
+            sprintf(name_msg, "%s %s", name, msg);
+            SSL_write(ssl, name_msg, strlen(name_msg));
+
+            // open the file and send the contents
+            FILE *fp = fopen(filename, "rb");
+            if (!fp) {
+                perror("file open error");
+                continue;
+            }
+
+            // get file size
+            fseek(fp, 0, SEEK_END);
+            long fsize = ftell(fp);
+            fseek(fp, 0, SEEK_SET);
+
+            // send file size
+            long fsize_net = htonll((uint64_t)fsize);
+            SSL_write(ssl, (char *)&fsize_net, sizeof(uint64_t));
+
+            // send file data
+            char fbuf[BUF_SIZE];
+            size_t nread;
+            while ((nread = fread(fbuf, 1, sizeof(fbuf), fp)) > 0) {
+                SSL_write(ssl, fbuf, nread);
+            }
+            fclose(fp);
+            printf("File [%s] sent successfully!\n", filename);
+        } else {    
+		    sprintf(name_msg, "%s %s", name, msg);
+            SSL_write(ssl, name_msg, strlen(name_msg));
+        }
 	}
-	return NULL;
+	
+    return NULL;
 }
 	
 void *recv_msg(void * arg)   // read thread main
@@ -69,14 +159,63 @@ void *recv_msg(void * arg)   // read thread main
 	int sock = *((int*)arg);
 	char name_msg[NAME_SIZE+BUF_SIZE];
 	int str_len;
+
 	while (1)
 	{
-		str_len = read(sock, name_msg, NAME_SIZE+BUF_SIZE-1);
-		if (str_len == -1) 
+        // receive data over TLS
+		str_len = SSL_read(ssl, name_msg, NAME_SIZE+BUF_SIZE-1);
+		if (str_len <= 0) 
 			return (void*)-1;
 		name_msg[str_len] = 0;
-		fputs(name_msg, stdout);
-	}
+
+        if (strstr(name_msg, "file_share:") != NULL) {
+            char *p = strstr(name_msg, "file_share:");
+            char filename[BUF_SIZE];
+            if (sscanf((p+11), "%s", filename) != 1) {
+                fputs("Error parsing filename.\n", stderr);
+                continue;
+            }
+
+            // read file size
+            uint64_t fsize_net;
+            if (SSL_read(ssl, &fsize_net, sizeof(uint64_t)) <= 0) {
+                fputs("Error reading file size.\n", stderr);
+                continue;
+            }
+            uint64_t fsize = ntohll(fsize_net);
+
+            // receive the file data
+            FILE *fp = fopen(filename, "wb");
+            if (!fp) {
+                fputs("File open error.\n", stdout);
+                // still must read the data to clear the SSL buffer
+                char dummy[BUF_SIZE];
+                uint64_t remaining = fsize;
+                while (remaining > 0) {
+                    int toread = (remaining > sizeof(dummy)) ? sizeof(dummy) : remaining;
+                    int n = SSL_read(ssl, dummy, toread);
+                    if (n <= 0) break;
+                    remaining -= n;
+                }
+                continue;
+            }
+
+            uint64_t remaining = fsize;
+            while (remaining > 0) {
+                char fbuf[BUF_SIZE];
+                int toread = (remaining > sizeof(fbuf)) ? sizeof(fbuf) : remaining;
+                int n = SSL_read(ssl, fbuf, toread);
+                if (n <= 0) break;
+                fwrite(fbuf, 1, n, fp);
+                remaining -= n;
+            }
+            
+            fclose(fp);
+            printf("File [%s] received succesfully!\n", filename);
+        } else {
+            fputs(name_msg, stdout);
+        }
+    }
 	return NULL;
 }
 	
--- hw3/tls_chat_serv.c
+++ hw3/tls_chat_serv.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
@@ -6,17 +7,26 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <pthread.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
 
-#define BUF_SIZE 100
+#define BUF_SIZE 1024
 #define MAX_CLNT 256
+
+#define htonll(x)   ((((uint64_t)htonl(x)) << 32) + htonl(x >> 32))
+#define ntohll(x)   ((((uint64_t)ntohl(x)) << 32) + ntohl(x >> 32))
 
 void *handle_clnt(void * arg);
 void send_msg(char * msg, int len);
+void broadcast_file(void *data, int len);
 void error_handling(char * msg);
 
 int clnt_cnt = 0;
 int clnt_socks[MAX_CLNT];
+SSL *ssl_list[MAX_CLNT]; // store SSL pointers for each client
 pthread_mutex_t mutx;
+
+SSL_CTX *ctx;
 
 int main(int argc, char *argv[])
 {
@@ -24,10 +34,32 @@ int main(int argc, char *argv[])
 	struct sockaddr_in serv_adr, clnt_adr;
 	int clnt_adr_sz;
 	pthread_t t_id;
+
 	if (argc != 2) {
 		printf("Usage : %s <port>\n", argv[0]);
 		exit(1);
 	}
+    
+    // initialize OpenSSL
+    SSL_library_init();
+    OpenSSL_add_all_algorithms();
+    SSL_load_error_strings();
+    const SSL_METHOD *method = TLS_server_method();
+    ctx = SSL_CTX_new(method);
+    if (!ctx) {
+        error_handling("SSL_CTX_new() error");
+    }
+
+    // load server certificate and key
+    if (SSL_CTX_use_certificate_file(ctx, "server.crt", SSL_FILETYPE_PEM) <= 0) {
+        error_handling("Certificate error");
+    }
+    if (SSL_CTX_use_PrivateKey_file(ctx, "server.key", SSL_FILETYPE_PEM) <= 0) {
+        error_handling("PrivateKey error");
+    }
+    if (!SSL_CTX_check_private_key(ctx)) {
+        error_handling("Private key does not match the certificate public key");
+    }
   
 	pthread_mutex_init(&mutx, NULL);
 	serv_sock = socket(PF_INET, SOCK_STREAM, 0);
@@ -45,17 +77,37 @@ int main(int argc, char *argv[])
 	while (1)
 	{
 		clnt_adr_sz = sizeof(clnt_adr);
-		clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_adr,&clnt_adr_sz);
-		
+		clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_adr, &clnt_adr_sz);
+        if (clnt_sock == -1) continue;
+
+        // create SSL object and accept
+        SSL *ssl = SSL_new(ctx); // create a new SSL object
+        SSL_set_fd(ssl, clnt_sock); // associate SSL with the socket
+                                
+        // perform TLS handshake with the client
+        if (SSL_accept(ssl) <= 0) {
+            ERR_print_errors_fp(stderr);
+            close(clnt_sock);
+            SSL_free(ssl);
+            continue;
+        }
+        printf("TLS handshake successful!\n");
+	    printf("Cipher: %s\n", SSL_get_cipher(ssl));
+        printf("TLS Version: %s\n", SSL_get_version(ssl));
+
 		pthread_mutex_lock(&mutx);
 		clnt_socks[clnt_cnt++] = clnt_sock;
+        ssl_list[clnt_cnt] = ssl;
+        clnt_cnt++;
 		pthread_mutex_unlock(&mutx);
 	
 		pthread_create(&t_id, NULL, handle_clnt, (void*)&clnt_sock);
 		pthread_detach(t_id);
 		printf("Connected client IP: %s \n", inet_ntoa(clnt_adr.sin_addr));
 	}
+
 	close(serv_sock);
+    SSL_CTX_free(ctx);
 	return 0;
 }
 	
@@ -64,39 +116,145 @@ void *handle_clnt(void * arg)
 	int clnt_sock = *((int*)arg);
 	int str_len = 0, i;
 	char msg[BUF_SIZE];
-	
-	while ((str_len = read(clnt_sock, msg, sizeof(msg))) != 0)
-		send_msg(msg, str_len);
-	
+    SSL *ssl = NULL;
+
+    // find the corresponding SSL for this client socket
+    pthread_mutex_lock(&mutx);
+    for (i = 0; i < clnt_cnt; i++) {
+        if (clnt_socks[i] == clnt_sock) {
+            ssl = ssl_list[i];
+            break;
+        }
+    }
+    pthread_mutex_unlock(&mutx);
+    
+    int ret = SSL_read(ssl, msg, sizeof(msg));
+    if (ret <= 0) {
+        int err = SSL_get_error(ssl, ret);
+        switch (err) {
+            case SSL_ERROR_ZERO_RETURN:
+                // The TLS/SSL peer has closed the connection
+                fprintf(stderr, "SSL_read: Connection closed by peer.\n");
+                break;
+            case SSL_ERROR_WANT_READ:
+            case SSL_ERROR_WANT_WRITE:
+                // Non-blocking operation: SSL_read needs to be called again later
+                fprintf(stderr, "SSL_read: Non-blocking retry needed.\n");
+                break;
+            case SSL_ERROR_SYSCALL:
+                // Check errno or additional error info
+                perror("SSL_read: I/O error");
+                break;
+            case SSL_ERROR_SSL:
+                // A failure in the SSL library occurred, print details
+                fprintf(stderr, "SSL_read: SSL protocol error.\n");
+                ERR_print_errors_fp(stderr);
+                break;
+            default:
+                fprintf(stderr, "SSL_read: Unexpected error code %d.\n", err);
+                ERR_print_errors_fp(stderr);
+                break;
+        }
+        // Depending on err, you may need to clean up and close the connection
+    }    
+
+    while ((str_len = SSL_read(ssl, msg, sizeof(msg))) > 0)
+    {
+        msg[str_len] = 0;
+        printf("Received: %s\n", msg);
+
+        // check if command is file_share
+        if (strstr(msg, "file_share:") != NULL) {
+            // read file size
+            uint64_t fsize_net;
+            if (SSL_read(ssl, &fsize_net, sizeof(uint64_t)) <= 0)
+                break;
+            uint64_t fsize = ntohll(fsize_net);
+
+            // Read file data
+            char *file_data = (char*)malloc(fsize);
+            if (!file_data) {
+                // If memory alloc fails, just discard data
+                char dummy[BUF_SIZE];
+                uint64_t remaining = fsize;
+                while (remaining>0) {
+                    int toread = (remaining > sizeof(dummy)) ? sizeof(dummy) : remaining;
+                    int n = SSL_read(ssl,dummy,toread);
+                    if (n <= 0) break;
+                    remaining -= n;
+                }
+                continue;
+            }
+
+            uint64_t remaining = fsize;
+            char fbuf[BUF_SIZE];
+            char *ptr = file_data;
+            while (remaining > 0) {
+                int toread = (remaining > sizeof(fbuf)) ? sizeof(fbuf) : remaining;
+                int n = SSL_read(ssl, fbuf, toread);
+                if (n <= 0) { 
+                    free(file_data); 
+                    goto cleanup;
+                }
+                memcpy(ptr, fbuf, n);
+                ptr += n;
+                remaining -= n;
+            }
+
+            // broadcast the initial line (with file_share:)
+            send_msg(msg, str_len);
+            // broadcast the file size
+            broadcast_file((char*)&fsize_net, sizeof(uint64_t));
+            // Broadcast the file data
+            broadcast_file(file_data, fsize);
+            free(file_data);
+        } else {
+            send_msg(msg, str_len);
+        }
+    }
+
+cleanup:
 	pthread_mutex_lock(&mutx);
 	for (i = 0; i < clnt_cnt; i++)   // remove disconnected client
 	{
 		if (clnt_sock == clnt_socks[i])
 		{
-			while (i < clnt_cnt-1)
-			{
-				clnt_socks[i] = clnt_socks[i+1];
-				i++;
+            SSL_free(ssl_list[i]);
+            // shift remaining clients
+            for (int j = i; j < clnt_cnt - 1; j++) {
+				clnt_socks[j] = clnt_socks[j+1];
+                ssl_list[j] = ssl_list[j+1];
 			}
+            clnt_cnt--;
+            printf("Number of current connected clients: %d\n", clnt_cnt);
 			break;
 		}
 	}
-	clnt_cnt--;
 	pthread_mutex_unlock(&mutx);
 	close(clnt_sock);
 	return NULL;
 }
+
 void send_msg(char * msg, int len)   // send to all
 {
-	int i;
 	pthread_mutex_lock(&mutx);
-	for (i = 0; i < clnt_cnt; i++)
-		write(clnt_socks[i], msg, len);
+	for (int i = 0; i < clnt_cnt; i++)
+		SSL_write(ssl_list[i], msg, len);
 	pthread_mutex_unlock(&mutx);
 }
+
+void broadcast_file(void *data, int len) 
+{
+    pthread_mutex_lock(&mutx);
+    for (int i = 0; i < clnt_cnt; i++) {
+        SSL_write(ssl_list[i], data, len);
+    }
+    pthread_mutex_unlock(&mutx);
+}
+
 void error_handling(char * msg)
 {
 	fputs(msg, stderr);
 	fputc('\n', stderr);
-	exit(1);
+	exit(EXIT_FAILURE);
 }
